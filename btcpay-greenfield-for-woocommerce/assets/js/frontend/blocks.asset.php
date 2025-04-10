8aa124e8487d070c4542479e9bdded5875d5ae45
Sean Murphy <sean@iamseanmurphy.com>
2025-04-10 14:18:08 +0000

    2.7.2

--- btcpay-greenfield-for-woocommerce/assets/js/frontend/blocks.asset.php
+++ btcpay-greenfield-for-woocommerce/assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'f27172b7905ad8eb456218bc4613cd3a');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '9ef846edac3af0fea94e7121d0397336');
